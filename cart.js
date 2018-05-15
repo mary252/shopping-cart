eac56569cec9166aafca00472518aa95c73c51ff
mary252 <34661289+mary252@users.noreply.github.com>
2018-05-15 21:56:46 +0200

    cart finished

--- cart.js
+++ cart.js
@@ -6,38 +6,97 @@ function Product(ID,Name,Price){
     this.Price=Price;
 }
 
-Product.prototype.toString = function() {
-
-    return `${this.ID}, ${this.Name}, ${this.Price}`;
-};
-
 function Item (product,count){
 
     this.Product=product,
     this.Count=count;
     this.TotalPrice=product.Price * count;
 }
+Item.prototype.UpdatePrice = function () {
+    this.TotalPrice=this.Product.Price*this.Count;
+}
 
-Item.prototype.toString = function() {
-
-    var string=this.Product.toString();
-    return string +`, ${this.Count}, ${this.TotalPrice}`;
-};
-
-class Cart{
+export class Cart{
 
     constructor(){
-        var items=[];
+        this.items=[];
     }
+
+     // returns all items in the cart
     GetItems(){
         return this.items;
     }
 
-}
-export class {Cart} ;
+    // checks if cart contains an item with this product by id return Boolean
+    HasProduct(Product){
+        for(let i=0;i<this.items.length;i++){
+            if(Product.ID==this.items[i].Product.ID){
+                return true;
+            }
+        }
+        return false;
+    }
 
-const bread=new Product(1,'bread',12);
-bread.toString();
-console.log(bread.toString());
-const breaditem = new Item(bread,13);
-console.log(breaditem.toString());
+    // adds a product or increment the item count
+    AddItem(product){
+        
+        for(let i=0;i<this.items.length;i++){
+            if(product.ID==this.items[i].Product.ID){
+                this.items[i].Count++;
+                this.items[i].UpdatePrice();
+                return;
+            }
+        }
+        var ItemToAdd=new Item(product,1);
+        this.items.push(ItemToAdd);
+    }
+    
+    // decrement the count of the item containing the product or removes it.
+    RemoveItem(product){
+        for(let i=0;i<this.items.length;i++){
+            if(product.ID==this.items[i].Product.ID){
+                this.items[i].Count--;
+                this.items[i].UpdatePrice();
+                if(this.items[i].Count==0){
+                    this.items.splice(i,1);
+                }
+            }
+        }
+    }
+
+     // removes the item that contains the product 
+    RemoveProduct(product){
+        for(let i=0;i<this.items.length;i++){
+            if(product.ID==this.items[i].Product.ID){
+                this.items.splice(i,1);
+                
+            }
+        }
+    } 
+
+    // get an item containing the product or null otherwise
+    GetItem(product){
+        for(let i=0;i<this.items.length;i++){
+            if(product.ID==this.items[i].Product.ID){
+                return this.items[i];
+            }
+        }
+        return null;
+    } 
+
+    // returns the total of all items in the cart.
+    GetTotal(){
+        var TotalPrice=0;
+        for(let i=0;i<this.items.length;i++){
+           TotalPrice+=this.items[i].TotalPrice;
+        }
+        return TotalPrice;
+    }
+
+    // removes all items
+    ClearCart(){
+        this.items=[];
+    }
+
+
+}
